3f1879a71600318edb73c8f5849aad75064c14bb
seungho-jung <minttmdgh@naver.com>
2022-08-29 09:59:37 +0900

    additional

---
+++ relative_ADP.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import pandas as pd
+#relative_ADP="."+"/"+"realtive_ADP"+".txt"
+#f = open(relative_ADP, 'a')
+for sample_num in range(1,21):
+    sample_num_add = sample_num*50000
+    for j in range(8,9):
+        corrname="./attack_result"+"/"+str(sample_num_add)+"corr_"+str(j)+".txt"
+        df_1 = pd.read_table(corrname ,sep=' ', header=None, names=['correlation'])
+        print(df_1)
+        x = df_1.loc[35]/0.01*100
+#        print(x)
+
+#f.close()
