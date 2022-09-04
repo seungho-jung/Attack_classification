41e19e0539159d2d35a1997fa5416f325f988033
seungho-jung <minttmdgh@naver.com>
2022-09-05 03:24:22 +0900

    modified at 0905

--- Kutosis.py
+++ Kutosis.py
@@ -3,14 +3,17 @@
 import pandas as pd
 for sample_num in range(1,21): #21
     #sample_num_add = sample_num*50000
-    sample_num_add = sample_num*1000 #for L3
+    sample_num_add = sample_num*50000 #for L1
+    #sample_num_add = sample_num*1000 #for L3
     #kurtosis_name="./kurtosis_result"+"/"+"kurtosis_"+str(sample_num_add)+".txt"
-    kurtosis_name="./L3_kurtosis_result"+"/"+"kurtosis_"+str(sample_num_add)+".txt"
+    kurtosis_name="./L1_kurtosis_result"+"/"+"kurtosis_"+str(sample_num_add)+".txt"
+    #kurtosis_name="./L3_kurtosis_result"+"/"+"kurtosis_"+str(sample_num_add)+".txt"
 
     f = open(kurtosis_name, 'w')
     for byte_num in range(0,16):
     #    foldername="./attack_result"+"/"+str(sample_num_add)+"corr_"+str(byte_num)+".txt"
-        foldername="./L3_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(byte_num)+".txt"
+        foldername="./L1_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(byte_num)+".txt"
+        #foldername="./L3_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(byte_num)+".txt"
         df=pd.read_table(foldername,sep=' ', header=None, names=['correlation'])
         corr=df[['correlation']]
         corr = corr.head(256)
--- modified_attack.py
+++ modified_attack.py
@@ -2,12 +2,13 @@
 
 import pandas as pd
 for k in range(1,21):
-    #k_1 = k*50000
-    k_1 = k*1000 #for L3
+    k_1 = k*50000 #for L1
+    #k_1 = k*1000 #for L3
     for j in range(0,16):
         foldername="./memory_request/byte"+str(j)
         #corrname="./attack_result"+"/"+str(k_1)+"corr_"+str(j)+".txt"
-        corrname="./L3_correlation_result"+"/"+str(k_1)+"corr_"+str(j)+".txt"
+        corrname="./L1_correlation_result"+"/"+str(k_1)+"corr_"+str(j)+".txt"
+        #corrname="./L3_correlation_result"+"/"+str(k_1)+"corr_"+str(j)+".txt"
         f = open(corrname, 'w')
         for i in range(0, 256):
             filename =foldername+"/key"+str(i)
@@ -15,7 +16,8 @@ for k in range(1,21):
             x = df_1[['mem_request']]
             x = x.head(k_1)
             #df_2 = pd.read_table('./cycle_data/cycle_default',sep=' ', header=None, names=['sample', 'cycle'])
-            df_2 = pd.read_table('./L3_cycle/output_tot_cycle_DRAM.txt',sep=' ', header=None, names=['sample', 'cycle'])
+            df_2 = pd.read_table('./L1_cycle/output_tot_cycle_L1.txt',sep=' ', header=None, names=['sample', 'cycle'])
+            #df_2 = pd.read_table('./L3_cycle/output_tot_cycle_DRAM.txt',sep=' ', header=None, names=['sample', 'cycle'])
 
             y = df_2[['cycle']]
             y = y.head(k_1)
