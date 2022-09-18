2c76f5e782d82999cb5038ad6e1ff33e7882d75f
seungho-jung <minttmdgh@naver.com>
2022-09-19 00:56:16 +0900

    modified mem_hierarchy

--- excelmacro.py
+++ excelmacro.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import pandas as pd
-for sample_num in range(1,21):
+for sample_num in range(20,21):
+    memory_hierarchy = 'ghostleg'
 #    sample_num_add = sample_num*50000
 #    corrname_0="./attack_result"+"/"+str(sample_num_add)+"corr_"+str(0)+".txt"
 #    corrname_1="./attack_result"+"/"+str(sample_num_add)+"corr_"+str(1)+".txt"
-    sample_num_add = sample_num*1000 #for L1
-    corrname_0="./L1_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(0)+".txt"
-    corrname_1="./L1_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(1)+".txt"
+    sample_num_add = sample_num*50000 #for L1
+    corrname_0="./"+memory_hierarchy+"_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(0)+".txt"
+    corrname_1="./"+memory_hierarchy+"_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(1)+".txt"
 
     #sample_num_add = sample_num*1000 #for L3
     #corrname_0="./L3_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(0)+".txt"
@@ -16,7 +17,7 @@ for sample_num in range(1,21):
     df_1 = pd.read_table(corrname_1 ,sep=' ', header=None, names=['correlation'+str(1)])
     result = pd.concat([df_0,df_1],axis=1)
     for j in range(2,16):
-        corrname="./L1_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(j)+".txt"    
+        corrname="./"+memory_hierarchy+"_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(j)+".txt"    
         #corrname="./L3_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(j)+".txt"
         df = pd.read_table(corrname ,sep=' ', header=None, names=['correlation'+str(j)])
         result = pd.concat([result,df],axis=1)
