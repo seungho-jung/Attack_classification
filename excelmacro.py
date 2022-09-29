1b6e0e6d91273d8acafc58928ac9a8dc59acda8a
seungho-jung <minttmdgh@naver.com>
2022-09-30 03:11:24 +0900

    temporal

--- excelmacro.py
+++ excelmacro.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import pandas as pd
 for sample_num in range(20,21):
-    memory_hierarchy = 'ghostleg'
+    memory_hierarchy = 'mix_million'
 #    sample_num_add = sample_num*50000
 #    corrname_0="./attack_result"+"/"+str(sample_num_add)+"corr_"+str(0)+".txt"
 #    corrname_1="./attack_result"+"/"+str(sample_num_add)+"corr_"+str(1)+".txt"
-    sample_num_add = sample_num*50000 #for L1
+    sample_num_add = sample_num*500000 #for L1
     corrname_0="./"+memory_hierarchy+"_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(0)+".txt"
     corrname_1="./"+memory_hierarchy+"_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(1)+".txt"
 
