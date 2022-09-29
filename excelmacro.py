3fc69ef7b424d96515c3d2c0fe3284397cf6d432
seungho-jung <minttmdgh@naver.com>
2022-09-29 21:16:56 +0900

    modified at school

--- excelmacro.py
+++ excelmacro.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import pandas as pd
-for sample_num in range(20,21):
-    memory_hierarchy = 'ghostleg'
+for sample_num in range(1,10):
+    memory_hierarchy = 'width_32'
 #    sample_num_add = sample_num*50000
 #    corrname_0="./attack_result"+"/"+str(sample_num_add)+"corr_"+str(0)+".txt"
 #    corrname_1="./attack_result"+"/"+str(sample_num_add)+"corr_"+str(1)+".txt"
-    sample_num_add = sample_num*50000 #for L1
+    sample_num_add = sample_num*1000 #for L1
     corrname_0="./"+memory_hierarchy+"_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(0)+".txt"
     corrname_1="./"+memory_hierarchy+"_correlation_result"+"/"+str(sample_num_add)+"corr_"+str(1)+".txt"
 
--- modified_attack.py
+++ modified_attack.py
@@ -4,29 +4,30 @@ import pandas as pd
 from multiprocessing import Pool
 
 def cal_correlation(byte_num):
-    for k in range(1,21):
-        memory_hierarchy = 'L1'
-        k_1 = k*50000 #for L1
+    for k in range(1,11):
+        memory_hierarchy = 'width_32byte'
+        k_1 = k*1000 #for L1
         #k_1 = k*1000 #for L3
         foldername="./memory_request/byte"+str(byte_num)
         corrname="./"+memory_hierarchy+"_correlation_result"+"/"+str(k_1)+"corr_"+str(byte_num)+".txt"
         cyclename="./"+memory_hierarchy+"_cycle/output_tot_cycle_"+memory_hierarchy+".txt"
         f = open(corrname, 'w')
+
+        df_2 = pd.read_table(cyclename ,sep=' ', header=None, names=['sample', 'cycle'])
+        y = df_2[['cycle']]
+        #print(y)
+        y_temporal = y.head(k_1)
+        #print(y_temporal)
+
         for i in range(0, 256):
             filename =foldername+"/key"+str(i)
             df_1 = pd.read_table(filename ,sep=' ', header=None, names=['sample', 'mem_request'])
             x = df_1[['mem_request']]
-            x = x.head(k_1)
+            x_temporal = x.head(k_1)
             #df_2 = pd.read_table('./cycle_data/cycle_default',sep=' ', header=None, names=['sample', 'cycle'])
-            df_2 = pd.read_table(cyclename ,sep=' ', header=None, names=['sample', 'cycle'])
             #df_2 = pd.read_table('./L3_cycle/output_tot_cycle_DRAM.txt',sep=' ', header=None, names=['sample', 'cycle'])
 
-            y = df_2[['cycle']]
-            y = y.head(k_1)
-            df = pd.concat([x,y],axis =1)
-            x= df['mem_request']
-            y= df['cycle']
-            z= x.corr(y)
+            z= x_temporal['mem_request'].corr(y_temporal['cycle'])
             data = "%f\n" % z
             f.write(data)
         f.close()
